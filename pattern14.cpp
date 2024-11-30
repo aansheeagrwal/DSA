d825803f6f9f7f5bec7f00513131bc4a182b6cf8
aansheeagrwal <166138746+aansheeagrwal@users.noreply.github.com>
2024-11-30 23:33:17 +0530

    Add files via upload

---
+++ pattern14.cpp
@@ -0,0 +1,34 @@
+/*
+print the pattern 
+A
+A B
+A B C
+A B C D
+A B C D E
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void print14(int n) {
+
+    for(int i = 0;i<n;i++) {
+        // i = 2
+        for(char ch = 'A'; ch <= 'A' + i; ch++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i =0;i<t;i++) {
+        int n;
+        cin >> n;
+        print14(n);
+    }
+}
